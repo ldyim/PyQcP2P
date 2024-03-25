e84c6abed98f1509b9990d843bd892a6dcb20eb2
Ubuntu <ubuntu@ip-172-31-41-243.us-east-2.compute.internal>
2024-03-25 19:52:59 +0000

    asdf

--- temp2.py
+++ temp2.py
@@ -1,4 +1,5 @@
 from pythonp2p import Node
+import time
 HOST = "0.0.0.0"
 PORT = 9999
 FILE_PORT = 81
@@ -10,3 +11,6 @@ if __name__ == "__main__":
     
     #node.connect_to(ip, 65432)
     #node.send_message('{test}')   
+    time.sleep(10)
+    print("requesting e8834645a7c08dd0a8c07203f7385c4f")
+    node.requestFile("e8834645a7c08dd0a8c07203f7385c4f")
