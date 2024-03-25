5acac216451f5c63736bca783776eb0eedf6e2fa
Ubuntu <ubuntu@ip-172-31-30-139.us-east-2.compute.internal>
2024-03-25 19:20:58 +0000

    node2

---
+++ node2.py
@@ -0,0 +1,11 @@
+from pythonp2p import Node
+HOST = "0.0.0.0"
+PORT = 9998
+FILE_PORT = 65433
+ip = "172.31.41.243"
+if __name__ == "__main__":
+    print("starting main")
+    node = Node(HOST, PORT, FILE_PORT)  # start the node
+    node.start()
+    node.connect_to(ip, 9999)
+    node.send_message('{"test": "test message from node 3"}')
