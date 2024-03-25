962198038d594ffd868e72593c7ef4ef55a807c1
Ubuntu <ubuntu@ip-172-31-41-243.us-east-2.compute.internal>
2024-03-25 20:20:24 +0000

    asdf

--- 6675-1.py
+++ 6675-1.py
@@ -2,13 +2,12 @@ from pythonp2p import Node
 HOST = "0.0.0.0"
 PORT = 9998
 FILE_PORT = 65433
-ip = "172.31.41.243"
-private_ip = ""
+ip = "18.188.86.138"
+public_ip = "3.143.144.51"
 if __name__ == "__main__":
     print("starting main")
-    node = Node(HOST, PORT, FILE_PORT, private_ip)  # start the node
+    node = Node(HOST, PORT, FILE_PORT, public_ip)  # start the node
     node.start()
     node.connect_to(ip, 9999)
     node.send_message('{"test": "test message from node 3"}')
     print(node.addfile("test.txt"))
-    
