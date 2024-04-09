91f5d076956650da92086fed3b0146e579f36d6c
will.wei <will@lawn-10-91-119-156.lawn.gatech.edu>
2024-04-09 18:33:11 -0400

    pretest

--- pythonp2p/QUIC_FileTransfer/file_client.py
+++ pythonp2p/QUIC_FileTransfer/file_client.py
@@ -1,3 +1,48 @@
+# import asyncio
+# import sys
+
+# from aioquic.asyncio.protocol import QuicConnectionProtocol
+# from aioquic.asyncio.client import connect
+# from aioquic.quic.configuration import QuicConfiguration
+# from aioquic.quic.events import StreamDataReceived
+
+# class FileClientQuicProtocol(QuicConnectionProtocol):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.ready_event = asyncio.Event()
+
+#     def quic_event_received(self, event):
+#         if isinstance(event, StreamDataReceived):
+#             print(f"Get response from server: {event.data.decode()}")
+#             if event.data.endswith(b'\r\n'):
+#                 self.ready_event.set()
+
+# async def run_quic_client(server_ip, file_path):
+#     configuration = QuicConfiguration(is_client=True)
+#     configuration.load_verify_locations('server.crt')
+
+#     async with connect(
+#         server_ip, 4433, configuration=configuration, create_protocol=FileClientQuicProtocol
+#     ) as protocol:
+#         print('Connect to QUIC Server')
+#         # Read data from 
+#         with open(file_path, 'rb') as file:
+#             data = file.read()
+#         protocol._quic.send_stream_data(0, data)
+#         protocol._quic.send_stream_data(0, b'\r\n')
+#         protocol.transmit()
+#         # Wait for response from server
+#         await protocol.ready_event.wait() 
+
+# if __name__ == '__main__':
+#     if len(sys.argv) != 3:
+#         print("Command: python script.py <server_ip> <file_path>")
+#         sys.exit(1)
+
+#     server_ip = sys.argv[1]
+#     file_path = sys.argv[2]
+#     asyncio.run(run_quic_client(server_ip, file_path))
+
 import asyncio
 import sys
 
@@ -17,28 +62,24 @@ class FileClientQuicProtocol(QuicConnectionProtocol):
             if event.data.endswith(b'\r\n'):
                 self.ready_event.set()
 
-async def run_quic_client(server_ip, file_path):
-    configuration = QuicConfiguration(is_client=True)
-    configuration.load_verify_locations('server.crt')
+class QuicClient:
+    def __init__(self, server_ip, file_path):
+        self.server_ip = server_ip
+        self.file_path = file_path
+        self.configuration = QuicConfiguration(is_client=True)
+        self.configuration.load_verify_locations('server.crt')
 
-    async with connect(
-        server_ip, 4433, configuration=configuration, create_protocol=FileClientQuicProtocol
-    ) as protocol:
-        print('Connect to QUIC Server')
-        # Read data from 
-        with open(file_path, 'rb') as file:
-            data = file.read()
-        protocol._quic.send_stream_data(0, data)
-        protocol._quic.send_stream_data(0, b'\r\n')
-        protocol.transmit()
-        # Wait for response from server
-        await protocol.ready_event.wait() 
+    async def run(self):
+        async with connect(
+            self.server_ip, 4433, configuration=self.configuration, create_protocol=FileClientQuicProtocol
+        ) as protocol:
+            print('Connect to QUIC Server')
+            with open(self.file_path, 'rb') as file:
+                data = file.read()
+            protocol._quic.send_stream_data(0, data)
+            protocol._quic.send_stream_data(0, b'\r\n')
+            protocol.transmit()
+            await protocol.ready_event.wait() 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Command: python script.py <server_ip> <file_path>")
-        sys.exit(1)
-
-    server_ip = sys.argv[1]
-    file_path = sys.argv[2]
-    asyncio.run(run_quic_client(server_ip, file_path))
+    def start(self):
+        asyncio.run(self.run())
--- pythonp2p/QUIC_FileTransfer/file_server.py
+++ pythonp2p/QUIC_FileTransfer/file_server.py
@@ -1,3 +1,42 @@
+# import asyncio
+# from aioquic.asyncio.protocol import QuicConnectionProtocol
+# from aioquic.asyncio.server import serve
+# from aioquic.quic.configuration import QuicConfiguration
+# from aioquic.quic.events import StreamDataReceived, HandshakeCompleted
+
+# class FileServerQuicProtocol(QuicConnectionProtocol):
+    
+#     def quic_event_received(self, event):
+#         if isinstance(event, HandshakeCompleted):
+#             print("Handshake finished")
+#         elif isinstance(event, StreamDataReceived):
+#             print(f"Received data, buffering and write to file")
+#             with open('received_file.txt', 'wb') as file:
+#                 file.write(event.data)
+#                 if event.data.endswith(b'\r\n'):
+#                     print("File received successfully")
+#                     self.send_response()
+
+#     def send_response(self):
+#         response = b'File received successfully.\r\n'
+#         self._quic.send_stream_data(0, response, end_stream=True)
+#         self.transmit()
+#         print("Response sent to client.")
+
+# async def run_quic_server():
+#     configuration = QuicConfiguration(is_client=False)
+#     configuration.load_cert_chain('server.crt', 'server.key')
+
+#     await serve(
+#         '0.0.0.0', 4433, configuration=configuration, create_protocol=FileServerQuicProtocol
+#     )
+#     print('QUIC Server running on 0.0.0.0:4433')
+
+#     await asyncio.Event().wait()
+
+# if __name__ == '__main__':
+#     asyncio.run(run_quic_server())
+
 import asyncio
 from aioquic.asyncio.protocol import QuicConnectionProtocol
 from aioquic.asyncio.server import serve
@@ -5,7 +44,6 @@ from aioquic.quic.configuration import QuicConfiguration
 from aioquic.quic.events import StreamDataReceived, HandshakeCompleted
 
 class FileServerQuicProtocol(QuicConnectionProtocol):
-    
     def quic_event_received(self, event):
         if isinstance(event, HandshakeCompleted):
             print("Handshake finished")
@@ -23,16 +61,19 @@ class FileServerQuicProtocol(QuicConnectionProtocol):
         self.transmit()
         print("Response sent to client.")
 
-async def run_quic_server():
-    configuration = QuicConfiguration(is_client=False)
-    configuration.load_cert_chain('server.crt', 'server.key')
+class QuicServer:
+    def __init__(self, host='0.0.0.0', port=4433):
+        self.host = host
+        self.port = port
+        self.configuration = QuicConfiguration(is_client=False)
+        self.configuration.load_cert_chain('server.crt', 'server.key')
 
-    await serve(
-        '0.0.0.0', 4433, configuration=configuration, create_protocol=FileServerQuicProtocol
-    )
-    print('QUIC Server running on 0.0.0.0:4433')
+    async def run(self):
+        await serve(
+            self.host, self.port, configuration=self.configuration, create_protocol=FileServerQuicProtocol
+        )
+        print(f'QUIC Server running on {self.host}:{self.port}')
+        await asyncio.Event().wait()
 
-    await asyncio.Event().wait()
-
-if __name__ == '__main__':
-    asyncio.run(run_quic_server())
+    def start(self):
+        asyncio.run(self.run())
