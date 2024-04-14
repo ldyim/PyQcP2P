5f5e67ab4887d4516612b64e7878fcf76b17c581
Ray <ubuntu@ip-172-31-91-247.ec2.internal>
2024-04-14 19:20:25 +0000

    random filename

---
+++ QUIC_FileTransfer/file_server.py
@@ -0,0 +1,41 @@
+import asyncio
+from aioquic.asyncio.protocol import QuicConnectionProtocol
+from aioquic.asyncio.server import serve
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.events import StreamDataReceived, HandshakeCompleted
+import uuid
+class FileServerQuicProtocol(QuicConnectionProtocol):
+    
+    def quic_event_received(self, event):
+        if isinstance(event, HandshakeCompleted):
+            print("Handshake finished")
+        elif isinstance(event, StreamDataReceived):
+            print(f"Received data, buffering and write to file")
+            filename = uuid.uuid4()
+            filename += '.txt'
+            filepath = "transfer_directory/"
+            with open(filepath + filename, 'wb') as file:
+                file.write(event.data)
+                if event.data.endswith(b'\r\n'):
+                    print("File received successfully")
+                    self.send_response()
+
+    def send_response(self):
+        response = b'File received successfully.\r\n'
+        self._quic.send_stream_data(0, response, end_stream=True)
+        self.transmit()
+        print("Response sent to client.")
+
+async def run_quic_server():
+    configuration = QuicConfiguration(is_client=False)
+    configuration.load_cert_chain('server.crt', 'server.key')
+
+    await serve(
+        '0.0.0.0', 4433, configuration=configuration, create_protocol=FileServerQuicProtocol
+    )
+    print('QUIC Server running on 0.0.0.0:4433')
+
+    await asyncio.Event().wait()
+
+if __name__ == '__main__':
+    asyncio.run(run_quic_server())
